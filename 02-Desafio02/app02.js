e8726be7d5e988fcc1961e83f692347294c56161
Vilander Costa <vilander.costa@gmail.com>
2025-05-13 21:55:31 -0300

    Desafio 02

---
+++ 02-Desafio02/app02.js
@@ -0,0 +1,14 @@
+
+//operadores
+
+
+let soma = 5+5
+let subtracao = 10-5
+let multiplicacao = 2*6
+let divisao = 10/2
+console.log("soma="+soma+" subtracao="+subtracao+" multiplicacao="+multiplicacao+" divisao="+divisao)
+
+let modulo = 9%3
+let exponenciacao = 2**9
+
+console.log("\nModulo ="+modulo+" exponenciação="+exponenciacao)
