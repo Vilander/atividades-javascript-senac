a7a7dd63b5b408b00a0f841620057ebc2d38a1e3
Vilander Costa <vilander.costa@gmail.com>
2025-05-13 21:39:34 -0300

    Desafio 01

---
+++ 01-Desafio01/app01.js
@@ -0,0 +1,12 @@
+var nome = prompt("Informe seu nome", "Digite aqui seu nome");
+
+var idade = prompt("Informe sua idade", "Digite aqui sua idade");
+
+let endereco = prompt("Informe seu endereço","Digite seu endereço(Rua+número)")
+let cidade = prompt("Informe sua cidade","Digite sua cidade")
+
+
+/*console.log("Seja bem vindo " + nome + "!!! Você tem " + idade + " anos");*/
+
+alert("Seja bem vindo " + nome + "!!! Você tem " + idade + " anos. Você mora no endereço: " + endereco + ", situado na cidade de " + cidade);
+
